20249daac7222e440134d5b3cace4743c9d92029
Burhanuddin Indorewala <burhanuddinindorewala53@gmail.com>
2023-05-22 00:10:36 +0300

    added 2 questions to the quiz

---
+++ quiz_game.py
@@ -0,0 +1,18 @@
+answer = input("Hello! Would you like to play the computer quiz game? ").lower()
+if answer != 'yes':
+    quit()
+
+score = 0
+answer = input('What does CPU stand for? ').lower()
+if answer == 'central processing unit':
+    print("Correct!")
+    score += 1
+    
+answer = input('What does GPU stand for? ').lower()
+if answer == 'graphics processing unit':
+    print("Correct!")
+    score += 1
+
+print(score)
+print("You got {0} answer(s) correct.".format(str(score)))
+print("You got {0}%.".format((score / 2) * 100))
