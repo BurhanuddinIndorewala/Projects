d10233dd36525c6f8c08402f102a08bbd50fc2c8
Burhanuddin Indorewala <burhanuddinindorewala53@gmail.com>
2023-05-23 22:19:35 +0300

    no comments

---
+++ working with tkinter.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+# from tkinter import *
+window = tk.Tk()
+window.title("Burhan")
+window.geometry("300x300")
+
+# we store labels in variables
+label_1 = tk.Label(window, text="Hi", font=("Arial", 18))
+window.mainloop()
